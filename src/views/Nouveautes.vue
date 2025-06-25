a4b51ab5eea953d0dba5f3ab5cb45f147892f0e5
MAHEGwendal <mahegwendal8@gmail.com>
2025-06-25 09:25:30 +0200

    Ajout de la page nouveautés

--- src/views/Nouveautes.vue
+++ src/views/Nouveautes.vue
@@ -1,13 +1,13 @@
 <template>
   <Navbar />
   <div>
-    <h1>Nouveautés</h1>
-    <p>Découvrez ici les dernières nouveautés.</p>
+    <NewGames />
   </div>
   <Footer />
 </template>
 <script setup>
 import Navbar from '../components/Navbar.vue';
 import Footer from '../components/Footer.vue';
+import NewGames from '../components/NewGames.vue';
 </script>
 
